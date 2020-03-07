34e9fdfa46e06bb1e1d26006300d4f5e7868252d
Bogdan <bogdanstorozhuk96@gmail.com>
2020-03-07 17:26:04 +0200

    FL12_Webinar_HW7:fixes

--- FL12_Webinar_HW7/homework/webpack.config.js
+++ FL12_Webinar_HW7/homework/webpack.config.js
@@ -1,39 +1,69 @@
-const path = require("path");
-const webpack = require("webpack");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const isProduction = process.env.NODE_ENV === 'production';
-console.log(isProduction);
-const webpack_rules = [];
-const webpackOption = {
-    mode: 'production',
-    entry: "./js/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "app.js",
-    },
-    module: {
-        rules: webpack_rules
-    },
-    plugins: isProduction ? [new MiniCssExtractPlugin()] : []
-};
-let babelLoader = {
-    test: /\.js$/,
-    exclude: /(node_modules|bower_components)/,
-    use: {
-        loader: "babel-loader",
-        options: {
-            presets: ["@babel/preset-env"]
+const path = require("path"),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = (env = {}) => {
+    const {
+        mode = 'development'
+    } = env;
+    const isProd = mode === 'production',
+        isDev = mode === 'development';
+    const getStyleLoaders = () => {
+        return [isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+        ]
+    };
+    const getPlugins = () => {
+        const plugins = [new HtmlWebpackPlugin({
+            title: 'FL12_Webinar_HW7',
+            buildTime: new Date().toISOString(),
+            template: './index.html'
+        })];
+        if (isProd) {
+            plugins.push(new MiniCssExtractPlugin({
+                filename: './css/styles.css'
+            }));
         }
-    }
-};
-let lessLoader = {
-    test: /\.less$/,
-    use: [
-        isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 
-        'css-loader', 
-        'less-loader'
-    ]
+        return plugins;
+    };
+    return {
+        mode: isProd ? 'production' : isDev && 'development',
+        entry: "./js/index.js",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "./js/app.js",
+        },
+        module: {
+            rules: [{
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    loader: 'babel-loader'
+                },
+                //loading images
+                {
+                    test: /\.(png|jpg|jpeg|gif|ico)$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'img',
+                            name: '[name]-[sha1:hash:7].[ext]'
+                        }
+                    }]
+                },
+                //Loading css
+                {
+                    test: /\.(css)$/,
+                    use: getStyleLoaders()
+                },
+                //Loading Less
+                {
+                    test: /\.less$/,
+                    use: [...getStyleLoaders(), 'less-loader']
+                }
+            ]
+        },
+        plugins: getPlugins(),
+        devServer: {
+            open: true
+        }
+    };
 }
-webpack_rules.push(babelLoader);
-webpack_rules.push(lessLoader);
-module.exports = webpackOption;

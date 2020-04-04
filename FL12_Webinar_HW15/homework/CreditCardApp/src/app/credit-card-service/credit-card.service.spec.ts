e043b5d6f70394fb6fcdfbeb877a9590caab9bc8
Bogdan <bogdanstorozhuk96@gmail.com>
2020-04-04 14:13:52 +0300

    FL12_Webinar_HW15:final commit

--- FL12_Webinar_HW15/homework/CreditCardApp/src/app/credit-card-service/credit-card.service.spec.ts
+++ FL12_Webinar_HW15/homework/CreditCardApp/src/app/credit-card-service/credit-card.service.spec.ts
@@ -5,19 +5,19 @@ import { cardErrors } from './../static-data/card-errors';
 
 describe('CreditCardService', () => {
   let service: CreditCardService;
-  let message: string;
+
+  let [
+    VALID_CARD,
+    UNKNOWN_TYPE,
+    INVALID_NUMBER,
+    INVALID_NUMBER_FORMAT,
+    INVALID_LENGTH,
+    SCAM_ATTEMPT
+  ] = cardErrors;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(CreditCardService);
-    // [
-    //   this.VALID_CARD,
-    //   this.UNKNOWN_TYPE,
-    //   this.INVALID_NUMBER,
-    //   this.INVALID_NUMBER_FORMAT,
-    //   this.INVALID_LENGTH,
-    //   this.SCAM_ATTEMPT
-    // ] = cardErrors;
   });
 
   it('should create CreditCardService', async () => {
@@ -25,38 +25,66 @@ describe('CreditCardService', () => {
   });
 
   it('should return result with message:UNKNOWN_TYPE if card has unknown type', async () => {
-    message = service.testCreditCard('5500 0000 0000 0004', 'randomType').message;
-    expect(message).toEqual('Unknown card type');
+    const creditCardNumber = '5500 0000 0000 0004';
+    const invalidCardType = 'randomType';
+
+    const message = service.testCreditCard(creditCardNumber, invalidCardType).message;
+
+    expect(message).toEqual(UNKNOWN_TYPE);
   });
 
   it('should return result with message:INVALID_NUMBER_FORMAT if card number has incorrect format', async () => {
-    message = service.testCreditCard('340w 0000 0000 009', 'AmEx').message;
-    expect(message).toEqual('Credit card number is in invalid format');
+    const invalidCardNumber = '340w 0000 0000 009';
+    const creditCardType = 'AmEx';
+
+    const message = service.testCreditCard(invalidCardNumber, creditCardType).message;
+
+    expect(message).toEqual(INVALID_NUMBER_FORMAT);
   });
 
   it('should return result with message:INVALID_NUMBER if card number is not correct according to modulus 10 algorithm', async () => {
-    message = service.testCreditCard('3400 0000 0000 208', 'AmEx').message;
-    expect(message).toEqual('Credit card number is invalid');
+    const invalidCardNumber = '3400 0000 0000 208';
+    const creditCardType = 'AmEx';
+
+    const message = service.testCreditCard(invalidCardNumber, creditCardType).message;
+
+    expect(message).toEqual(INVALID_NUMBER);
   });
 
   it('should return result with message:INVALID_NUMBER if card number prefix is invalid', async () => {
-    message = service.testCreditCard('4400 0000 0000 008', 'AmEx').message;
-    expect(message).toEqual('Credit card number is invalid');
+    const invalidCardNumber = '4400 0000 0000 008';
+    const creditCardType = 'AmEx';
+
+    const message = service.testCreditCard(invalidCardNumber, creditCardType).message;
+
+    expect(message).toEqual(INVALID_NUMBER);
   });
 
   it('should return result with message:SCAM_ATTEMPT if card number scam attempt', async () => {
-    message = service.testCreditCard('5490 9977 7109 2064', 'MasterCard').message;
-    expect(message).toEqual('Warning! This credit card number is associated with a scam attempt');
+    const scamCardNumber = '5490 9977 7109 2064';
+    const creditCardType = 'MasterCard';
+
+    const message = service.testCreditCard(scamCardNumber, creditCardType).message;
+
+    expect(message).toEqual(SCAM_ATTEMPT);
   });
 
   it('should return result with message:INVALID_LENGTH if card number length is less than appropriate', async () => {
-    message = service.testCreditCard('3400 0000 0000 0091', 'AmEx').message;
-    expect(message).toEqual('Credit card number has an inappropriate number of digits');
+    const invalidLengthCardNumber = '3400 0000 0000 0091';
+    const creditCardType = 'AmEx';
+
+    const message = service.testCreditCard(invalidLengthCardNumber, creditCardType).message;
+
+    expect(message).toEqual(INVALID_LENGTH);
   });
 
   it('should return result with message:VALID_CARD if card number is completely verified', async () => {
-    message = service.testCreditCard('3400 0000 0000 009', 'AmEx').message;
-    expect(message).toEqual('Credit card has a valid format');
+    const validCardNumber = '3400 0000 0000 009';
+    const creditCardType = 'AmEx';
+
+    const message = service.testCreditCard(validCardNumber, creditCardType).message;
+
+    expect(message).toEqual(VALID_CARD);
   });
-  
+
 });
